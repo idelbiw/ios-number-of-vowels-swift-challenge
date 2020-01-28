eaaa3ace8ee76a6328bf0250ec6236d1d4a5b2d4
Dimitri Bouniol <dimitri.bouniol@lambda.com>
2020-01-27 23:13:35 -0500

    all done

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,26 @@
 import UIKit
 
-func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+func numberOfVowels(in string: String, isYAVowel: Bool) -> Int {
+var vowelCount: Int = 0
     
+if isYAVowel == false {
+    for character in string {
+        if character == "a" || character == "e" || character == "i" || character == "o" || character == "u" || character == "A" || character == "E" || character == "I" || character == "O" || character == "U" {
+            vowelCount += 1
+        }
+    }
+    print(vowelCount)
+    return vowelCount
+} else if isYAVowel == true {
+    for character in string {
+        if character == "a" || character == "e" || character == "i" || character == "o" || character == "u" || character == "y" || character == "A" || character == "E" || character == "I" || character == "O" || character == "U" || character == "Y" {
+            vowelCount += 1
+            }
+        }
+    }
+ print(vowelCount)
+ return vowelCount
 }
+
+numberOfVowels(in: "Finally, it seriously started to work", isYAVowel: true)
+numberOfVowels(in: "FINALLY, IT SERIOUSLY STARTED TO WORK", isYAVowel: true)
